2c0a9ebf2c0b2ebac89a8a9d8367ce9df3e9200d
Juan Carlos Calderón <150750774+Johnccg@users.noreply.github.com>
2024-11-21 10:34:25 -0600

    docs:Añadir dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,25 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+//Corrutinas
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+
+    //Fragment
+    implementation("androidx.fragment:fragment-ktx:1.5.0")
+
+    //Activity
+    implementation("androidx.activity:activity-ktx:1.5.0")
+
+    //Data binding
+    implementation("androidx.databinding:databinding-runtime:7.1.2")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+
+    // Parse
+    implementation("com.github.parse-community.Parse-SDK-Android:parse:4.3.0")
 }
